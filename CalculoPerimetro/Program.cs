4b7d2d08300369ff8f523592473ca91b7133cbd2
Yato2606 <david.quevedo@uabc.edu.mx>
2022-02-22 19:18:09 -0800

    Cambio por nuevo proyecto

---
+++ CalculoPerimetro/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculoPerimetro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=10, b=10, c=7;
+            int p = a+b+c;
+            Console.WriteLine("El perimetro del triangulo es: " +p);
+            Console.WriteLine("Precione cualquier tecla para continuar");
+            Console.ReadKey();
+        }
+    }
+}
