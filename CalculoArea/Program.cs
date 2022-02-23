63d9f7d6d934fe60bd2b7b85b58207eeba71febd
Yato2606 <david.quevedo@uabc.edu.mx>
2022-02-23 14:27:08 -0800

    Cambio por nuevo proyecto

---
+++ CalculoArea/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculoArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10, b = 10, c = 7;
+            int area = (b*a)/2;
+            Console.WriteLine("El area del triangulo es: "+area);
+            Console.WriteLine("Precione cualquier tecla para continuar");
+            Console.ReadKey();
+        }
+    }
+}
