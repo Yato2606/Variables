e0968b9b826124aa4417a26ab41b982abdcb7700
Yato2606 <david.quevedo@uabc.edu.mx>
2022-03-02 19:36:36 -0800

    Nueva practica

---
+++ String/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            string ciudad;
+            Console.WriteLine("Escribe tu nombre: ");
+            nombre=Console.ReadLine();
+            Console.WriteLine("Escribe tu ciudad: ");
+            ciudad=Console.ReadLine();
+            Console.WriteLine("Hola "+nombre+ " Bienvenido a " + ciudad);
+            Console.WriteLine("Presione cualquier tecla para continuar");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
---
+++ StringInt/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringInt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            string edad;
+            Console.WriteLine("Escribe tu nombre: ");
+            nombre=Console.ReadLine();
+            Console.WriteLine("Escribe tu edad: ");
+            edad = Console.ReadLine();
+            Console.WriteLine("Nombre: "+nombre+ " Edad: " + edad);
+            Console.WriteLine("Presione cualquier tecla para continuar");
+            Console.ReadKey();
+        }
+    }
+}
